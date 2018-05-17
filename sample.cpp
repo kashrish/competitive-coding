73719efdc0a15c309204c3842429fb0af9e24254
Kashyap <rkashyap@BANL1606582aa.local>
2018-05-17 09:29:10 +0530

    hello

---
+++ sample.cpp
@@ -0,0 +1,20 @@
+
+#include "bits/stdc++.h"
+//#include <bits/stdc++.h">
+using namespace std;
+
+int main(){
+
+	int x,y;
+	cin>>x>>y;
+
+	cout<<x+y<<endl;
+
+	cout<<x*x+y*y<<endl;
+
+
+	return 0;
+
+}
+
+
